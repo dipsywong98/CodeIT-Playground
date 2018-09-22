72bc5bd09db7e3f42839d5528dc5bc4224936a06
dipsywong98 <facebookarmyoffcal@gmail.com>
2018-09-22 11:11:13 +0800

    sub task 1 done

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ let ancesters = {}
 
 const minBroadcast = data => {
   initNodes(data)
-  console.log({children,parents,nodes})
+  // console.log({children,parents,nodes})
   generateAncesterList()
-  console.log(ancesters)
-  // reduceAncesterList()
+  // console.log(ancesters)
+  reduceAncesterList()
+  // console.log(ancesters)
+  let sendList = broadcastOut()
+  console.log(sendList)
+  return sendList
 }
 
 const registerNode = node => {
@@ -36,10 +40,10 @@ const initNodes = data => {
 
 const generateAncesterList = () => {
   let nodeSum = Object.keys(nodes).length
-  console.log(nodeSum,Object.keys(ancesters).length)
+  // console.log(nodeSum,Object.keys(ancesters).length)
   while(Object.keys(ancesters).length < nodeSum){
     const node = Object.keys(nodes).filter(node=>!(node in ancesters))[0]
-    console.log('find ancestor of which is not in list'+node)
+    // console.log('find ancestor of which is not in list'+node)
     findAncester(node)
   }
 }
@@ -63,6 +67,39 @@ const findAncester = (node) => {
   return ancesters[node]
 }
 
+const reduceAncesterList = () => {
+  for(node in nodes){
+    ancesters[node] = ancesters[node].reduce((prev, currv)=>(prev.indexOf(currv)===-1?[...prev,currv]:prev),[])
+  }
+}
+
+const broadcastOut = () => {
+  //mark the frequency of nodes
+  const marks = {}
+  const sendList = []
+  Object.keys(nodes).forEach(node=>{
+    ancesters[node].forEach(ancester=>{
+      marks[ancester] = marks[ancester] || 0
+      marks[ancester]++
+    })
+  })
+  while(notSentNodes = Object.keys(nodes).filter(node=>(nodes[node])).sort((a,b)=>marks[a]<marks[b])){
+    if(notSentNodes.length === 0)break
+    // console.log('push',notSentNodes[0])
+    sendList.push(notSentNodes[0])
+    sendNode(notSentNodes[0])
+  }
+  return sendList
+}
+
+const sendNode = (node)=> {
+  // console.log('sendnode',node)
+  nodes[node] = false
+  // console.log('chilren',children[node],nodes[children[node][0]])
+  children[node].forEach(c=>(nodes[c]&&sendNode(c)))
+  return true
+}
+
 let testCases = [
   {
     "data" : [ "A->B" , "A->C" , "B->D" , "E->F" ]
@@ -72,7 +109,10 @@ let testCases = [
   },
   {
     "data" : [ "A->B" , "C->B" , "B->D" , "D->F", "E->B",  "F->G","F->H","F->E"]
+  },
+  {
+    "data" : [ "A->B", "B->C", "C->A"]
   }
 ]
 
-minBroadcast(testCases[2].data)
+testCases.forEach(test=>minBroadcast(test.data))
